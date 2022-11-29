d554caa69b52640fff9530191f160aca19784c66
justinbrown11 <96081581+justinbrown11@users.noreply.github.com>
2022-11-28 23:15:20 -0700

    added routes

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dailyPageView, indexPageView, weeklyPageView, monthlyPageView
+from .views import dailyPageView, indexPageView, weeklyPageView, monthlyPageView, searchResultsPageView
 
 
 urlpatterns = [
-    path('daily/', dailyPageView, name= 'daily'),
-    path('weekly/', weeklyPageView, name= 'weekly'),
-    path('monthly/', monthlyPageView, name= 'monthly'),
-    path('', indexPageView, name= 'index')
+    path('daily/', dailyPageView, name = 'daily'),
+    path('weekly/', weeklyPageView, name = 'weekly'),
+    path('monthly/', monthlyPageView, name = 'monthly'),
+    path('food/search/', searchResultsPageView, name = 'searchFoodQuery'),
+    path('', indexPageView, name = 'index'),
 ]
--- tracking/views.py
+++ tracking/views.py
@@ -1,21 +1,53 @@
 from django.shortcuts import render
 
+# Import functions
+from .functions.searchFood import searchFood
+
 # Create your views here.
 
 def indexPageView(request):
-    return render(request,'tracking/index.html')
+    return render(request, 'tracking/index.html')
 
 def dailyPageView(request):
-    return render(request,'tracking/daily.html')
+    return render(request, 'tracking/daily.html')
+
+async def searchResultsPageView(request):
+    """
+    The page view with results from food searching
+    """
+    try:
+        # Grab query param from request
+        query = request.GET.__getitem__("query")
+
+        # Search food
+        data = await searchFood(query)
+
+        # Check for error
+        if (data['result'] == 1):
+            raise Exception("Error occured searching food")
+
+        # Set context
+        context = {
+            "foods": data['data']['foods']
+        }
+
+        return render(request, 'tracking/searchResults.html', context)
+
+    except Exception as e:
+        # Log error
+        print(e)
+
+        # Render error page
+        return render(request, 'tracking/error.html')
 
 def weeklyPageView(request):
-    return render(request,'tracking/weekly.html')
+    return render(request, 'tracking/weekly.html')
 
 def monthlyPageView(request):
-    return render(request,'tracking/monthly.html')
+    return render(request, 'tracking/monthly.html')
 
 def loginPageView(request):
-    return render(request,'tracking/login.html')
+    return render(request, 'tracking/login.html')
 
 def accountCreationPageView(request):
     return render(request, 'tracking/createAccount.html')
